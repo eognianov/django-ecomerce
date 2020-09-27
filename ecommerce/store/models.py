cca9cbc4d9c497fa1ecab61eead3a42e718452e0
eognianov <emilian_89@abv.bg>
2020-09-27 18:57:15 +0300

    register models to the admin.py

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -34,7 +34,7 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     data_added = models.DateTimeField(auto_now_add=True)
 
-class ShippingModel(models.Model):
+class ShippingAddres(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     address = models.CharField(max_length=200, null=False)
