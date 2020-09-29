6df411dc8922b9f5bc64f0bde8d0dc8c5296ab41
eognianov <emilian_89@abv.bg>
2020-09-29 17:31:15 +0300

    add to cart backend logic

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
+import json
+
 from .models import *
 
 def store(request):
@@ -31,5 +33,27 @@ def checkout(request):
     return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print(f'Action: {action} | ProductId: {productId}')
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
     return JsonResponse('Item was added', safe=False)
 
