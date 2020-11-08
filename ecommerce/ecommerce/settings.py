3c6ae263ad6b7169291fd546f54405bf1f516211
eognianov <emilian_89@abv.bg>
2020-11-08 16:40:48 +0200

    fix bug into settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    'default': {
+    # 'default': {
 
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
